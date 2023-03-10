ba9410a03848a9f919dabb37421ca31085de68d2
LE CLAINCHE ANAIS <anais.leclainche1@gmail.com>
2023-03-10 15:02:57 +0100

    initial commit

---
+++ projet_airtraffic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='travail sur la prevision du trafic aerien ',
+    author='Anais_Leclainche',
+    license='MIT',
+)
